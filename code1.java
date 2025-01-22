8c8a101eb3f016edb529fc9d52f2d1a4d2ec8e07
deepakgla16 <deepaksharma972079@gmail.com>
2025-01-22 22:32:28 +0530

    first code Assignment2 Level 1

---
+++ code1.java
@@ -0,0 +1,14 @@
+package Assignment2;
+
+import java.util.*;
+
+public class code1 {
+    public static void main(String[] args) {
+        int birthyearOfharry = 2000;
+        int currentYear = 2024;
+
+        int age = currentYear - birthyearOfharry;
+
+        System.out.println(age);
+    }
+}
